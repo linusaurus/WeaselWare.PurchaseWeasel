fc6f17bf014e5c976cc4274f06a4bd4b9d82067c
Richard Young <richard@weaselware.biz>
2019-02-21 10:04:43 -0800

    Bootstrapper CLeaned up and functional

--- PurchaseWeasel-7/Startup/Bootstrapper.cs
+++ PurchaseWeasel-7/Startup/Bootstrapper.cs
@@ -1,19 +1,21 @@
 ﻿using Autofac;
-using PurchaseSQLDB.DataAccess.Services;
-using PurchaseWeasel_7.ViewModels;
+using PurchaseWeasel.DataAccess.Services;
+using PurchaseWeasel.UI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PurchaseWeasel.UI;
 
-namespace PurchaseWeasel_7.Startup
+namespace PurchaseWeasel.UI.Startup
 {
     public class Bootstrapper
     {
         public IContainer Bootstrap()
         {
             var builder = new ContainerBuilder();
+
 
             builder.RegisterType<MainWindow>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
